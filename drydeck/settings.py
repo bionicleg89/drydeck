b130a68423f702371daf4c468b326f44572a0896
Elliot Gillum <bionicleg89@gmail.com>
2024-04-04 16:33:34 -0500

    Configure settings for PostGIS database using environment variables.

--- drydeck/settings.py
+++ drydeck/settings.py
@@ -81,8 +81,12 @@ ASGI_APPLICATION = "drydeck.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_HOST"),
+        "PORT": env("DATABASE_PORT"),
     }
 }
 
